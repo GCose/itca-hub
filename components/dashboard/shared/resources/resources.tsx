b6fef36c5e6ad151411b509f99720882a3380929
Goodness Adewuyi <goodnessadewuyi8@gmail.com>
2025-09-20 12:06:02 +0000

    Removed the recycle bin feature as well as bookmarks feature. Not needed for version 1

--- components/dashboard/shared/resources/resources.tsx
+++ components/dashboard/shared/resources/resources.tsx
@@ -5,7 +5,7 @@ import useResourceAdmin from '@/hooks/resources/use-resource-admin';
 import { ResourcesComponentProps } from '@/types/interfaces/resource';
 import ResourceTable from '@/components/dashboard/table/resource-table';
 import DashboardLayout from '@/components/dashboard/layout/dashboard-layout';
-import { Upload, Bookmark, Filter, Building2, Tag, Eye, Search } from 'lucide-react';
+import { Upload, Filter, Building2, Tag, Eye, Search } from 'lucide-react';
 import DashboardPageHeader from '@/components/dashboard/layout/dashboard-page-header';
 import ResourceTableSkeleton from '@/components/dashboard/skeletons/resource-table-skeleton';
 import ResourceFilterSkeleton from '@/components/dashboard/skeletons/resource-filter-skeleton';
@@ -122,13 +122,13 @@ const ResourcesComponent = ({ role, userData }: ResourcesComponentProps) => {
       dashboardTitle: 'Resource Library',
       actions: (
         <div className="flex flex-col gap-4 w-full md:flex-row sm:mt-0 space-x-3">
-          <Link
+          {/* <Link
             href="/student/resources/bookmarks"
             className="group inline-flex items-center rounded-lg bg-gradient-to-r from-blue-700 to-blue-600 px-4 py-2 text-sm font-medium text-white hover:from-blue-800 hover:to-blue-700 focus:outline-none focus:ring-blue-500 focus:ring-offset-2 transition-all duration-300 shadow-md hover:shadow-lg"
           >
             <Bookmark className="mr-2 h-4 w-4 transition-transform group-hover:translate-y-[-2px]" />
             Bookmarks
-          </Link>
+          </Link> */}
         </div>
       ),
     },
@@ -141,9 +141,9 @@ const ResourcesComponent = ({ role, userData }: ResourcesComponentProps) => {
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
         <DashboardPageHeader
           title={config.title}
+          actions={config.actions}
           subtitle={config.subtitle}
           description={config.description}
-          actions={config.actions}
         />
       </div>
 
